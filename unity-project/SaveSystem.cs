9e58628979cbe5982b3c90870664651893040fc4
flux <VELLOVESU0@GMAIL.COM>
2024-07-15 22:54:55 -0400

    Add files via upload

--- unity-project/SaveSystem.cs
+++ unity-project/SaveSystem.cs
@@ -6,23 +6,27 @@ using TMPro;
 using UnityEditor.UIElements;
 using System;
 using UnityEngine.UI;
+using UnityEditor.Search;
 
 // PDF Documentation of saving variables: file:///E:/Unity/Local%20Projects/CarDash/Assets/BayatGames/SaveGameFree/Documentation/Save%20Game%20Free%20Documentation%20-%20PDF%20Version.pdf
 
 public class SaveSystem : MonoBehaviour
 {
+
+    [SerializeField] private string DocumentationJustCopyAndPasteInBrowser = "file:///E:/Unity/Local%20Projects/CarDash/Assets/BayatGames/SaveGameFree/Documentation/Save%20Game%20Free%20Documentation%20-%20PDF%20Version.pdf";
     [SerializeField] private GameObject FramerateInput;
     [SerializeField] private GameObject AAStartupToggle;
     [SerializeField] private GameObject CPStartupToggle;
     [SerializeField] private GameObject AASpotifySwapToggle;
     [SerializeField] private GameObject EngineStatisticsSystem;
+    [SerializeField] private GameObject DashMonitorSystem;
     [SerializeField] private int FallbackFPS = 72;
     [SerializeField] private bool EditorLoadingAndSaving = false;
-    private int FPSLimit;
-    private bool AAonStartup;
-    private bool CPonStartup;
-    private bool AASpotifySwap;
-    private bool vGearbox;
+    public int FPSLimit;
+    public bool AAonStartup;
+    public bool CPonStartup;
+    public bool AASpotifySwap;
+    public bool displayMode;
 
     void Start()
     {
@@ -54,11 +58,13 @@ public class SaveSystem : MonoBehaviour
         AAonStartup = SaveGame.Load<bool>("aa_on_startup");
         CPonStartup = SaveGame.Load<bool>("cp_on_startup");
         AASpotifySwap = SaveGame.Load<bool>("aa_spotify_swap");
-        vGearbox = SaveGame.Load<bool>("virtual_gearbox");
+        displayMode = SaveGame.Load<bool>("display_mode");
 
+        FramerateInput.GetComponent<TMP_InputField>().text = FPSLimit.ToString();
         AAStartupToggle.GetComponent<Toggle>().isOn = AAonStartup;
         CPStartupToggle.GetComponent <Toggle>().isOn = CPonStartup;
         AASpotifySwapToggle.GetComponent<Toggle>().isOn = AASpotifySwap;
+        DashMonitorSystem.GetComponent<DashMonitorController>().displayMode = displayMode;
     }
 
     public void CheckSaveFile ()
@@ -79,6 +85,7 @@ public class SaveSystem : MonoBehaviour
             AAonStartup = AAStartupToggle.GetComponent<Toggle>().isOn;
             CPonStartup = CPStartupToggle.GetComponent<Toggle>().isOn;
             AASpotifySwap = AASpotifySwapToggle.GetComponent <Toggle>().isOn;
+            displayMode = DashMonitorSystem.GetComponent<DashMonitorController>().displayMode;
 
             SaveGame.Save<bool>("has_ran", true);
             if (FPSLimit != -1 || FPSLimit! < 15)
@@ -95,7 +102,7 @@ public class SaveSystem : MonoBehaviour
             SaveGame.Save<bool>("aa_on_startup", AAonStartup);
             SaveGame.Save<bool>("cp_on_startup", CPonStartup);
             SaveGame.Save<bool>("aa_spotify_swap", AASpotifySwap);
-            SaveGame.Save<bool>("virtual_gearbox", EngineStatisticsSystem.GetComponent<EngineStatisticsController>().virtualGearboxStatus());
+            SaveGame.Save<bool>("display_mode", displayMode);
         }
     }
 
@@ -121,7 +128,7 @@ public class SaveSystem : MonoBehaviour
         }
         catch (FormatException e)
         {
-
+            Debug.LogError("[SaveSystem.cs] User inputted a alphabetic FPS value? {"+e+"}");
             FPSLimit = FallbackFPS;
         }
 
@@ -217,6 +224,27 @@ public class SaveSystem : MonoBehaviour
                 SaveGame.Save<bool>("aa_on_startup", false);
                 AAonStartup = SaveGame.Load<bool>("aa_on_startup");
             }
+        }
+    }
+
+    public void SaveDisplayMode()
+    {
+
+        if (Application.isEditor && !EditorLoadingAndSaving)
+        {
+
+            return;
+        }
+        else if (!SaveGame.Exists("has_ran"))
+        {
+
+            CheckSaveFile();
+        }
+
+        if (displayMode != DashMonitorSystem.GetComponent<DashMonitorController>().displayMode) // user changed variable
+        {
+            displayMode = DashMonitorSystem.GetComponent<DashMonitorController>().displayMode;
+            SaveGame.Save<bool>("display_mode", displayMode);
         }
     }
 }
